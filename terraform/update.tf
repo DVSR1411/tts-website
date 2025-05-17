a348f8c77deb5b9e9338e1d61658800486863c75
DANDA VENKATA SATHWIK REDDY <165892101+DVSR1411@users.noreply.github.com>
2025-05-17 21:04:18 +0530

    Update update.tf

--- terraform/update.tf
+++ terraform/update.tf
@@ -1,8 +1,8 @@
 resource "null_resource" "update_api" {
   provisioner "local-exec" {
     command = <<EOF
-    url = "${aws_api_gateway_stage.api_stage.invoke_url}/test"
-    file = "${path.module}/../frontend/script.js"
+    url="${aws_api_gateway_stage.api_stage.invoke_url}/test"
+    file="${path.module}/../frontend/script.js"
     sed -i "s|MyAPI|$url|g" "$file"
     echo "API URL updated"
     EOF
